9f9c26179e15b7af36b0a426c1797d518d3c2ec2
dcavadia <dan.acppp@gmail.com>
2023-04-14 14:24:28 -0400

    add environment & player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using Unity.Netcode;
+using UnityEngine;
+
+/// <summary>
+/// Simple character controller for movement
+/// </summary>
+public class PlayerController : NetworkBehaviour
+{
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    #region Movement
+
+    [SerializeField] private float acceleration = 80;
+    [SerializeField] private float maxVelocity = 10;
+    private Vector3 playerInput;
+    private Rigidbody rigidBody;
+
+    private void HandleMovement()
+    {
+        rigidBody.velocity += playerInput.normalized * (acceleration * Time.deltaTime);
+        rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxVelocity);
+    }
+
+    #endregion
+
+    #region Rotation
+
+    [SerializeField] private float rotationSpeed = 450;
+    private Plane groundPlane = new(Vector3.up, Vector3.zero);
+    private Camera mainCamera;
+
+    private void HandleRotation()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (groundPlane.Raycast(ray, out var enter))
+        {
+            var hitPoint = ray.GetPoint(enter);
+
+            var dir = hitPoint - transform.position;
+            var rot = Quaternion.LookRotation(dir);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    #endregion
+}
